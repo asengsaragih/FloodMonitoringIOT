19453cf344fb623424bb5021d2c8f45fa2d54053
Aldi Wahyu Saragih <49329871+asengsaragih@users.noreply.github.com>
2020-03-30 21:44:38 +0700

    Update rf95_client.cpp

--- rf95_client.cpp
+++ rf95_client.cpp
@@ -18,34 +18,40 @@
 
 #include <RH_RF69.h>
 #include <RH_RF95.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
 
 // define hardware used change to fit your need
-// Uncomment the board you have, if not listed 
+// Uncomment the board you have, if not listed
 // uncommment custom board and set wiring tin custom section
 
-// LoRasPi board 
+// LoRasPi board
 // see https://github.com/hallard/LoRasPI
-#define BOARD_LORASPI
+//#define BOARD_LORASPI
 
 // iC880A and LinkLab Lora Gateway Shield (if RF module plugged into)
 // see https://github.com/ch2i/iC880A-Raspberry-PI
 //#define BOARD_IC880A_PLATE
 
-// Raspberri PI Lora Gateway for multiple modules 
+// Raspberri PI Lora Gateway for multiple modules
 // see https://github.com/hallard/RPI-Lora-Gateway
 //#define BOARD_PI_LORA_GATEWAY
 
 // Dragino Raspberry PI hat
 // see https://github.com/dragino/Lora
-//#define BOARD_DRAGINO_PIHAT
+#define BOARD_DRAGINO_PIHAT
 
-// Now we include RasPi_Boards.h so this will expose defined 
+// Now we include RasPi_Boards.h so this will expose defined
 // constants with CS/IRQ/RESET/on board LED pins definition
 #include "../RasPiBoards.h"
 
-// Our RFM95 Configuration 
-#define RF_FREQUENCY  868.00
-#define RF_GATEWAY_ID 1 
+// Our RFM95 Configuration
+#define RF_FREQUENCY  915.00
+#define RF_GATEWAY_ID 1
 #define RF_NODE_ID    10
 
 // Create an instance of a driver
@@ -64,17 +70,23 @@ void sig_handler(int sig)
 //Main Function
 int main (int argc, const char* argv[] )
 {
+  std::string line;
+  std::string line2;
+  std::string new1;
+  std::string new2;
+  std::string new3;
+  std::string line3;
+  //ifstream myfile ("/home/pi/gps.txt");
   static unsigned long last_millis;
   static unsigned long led_blink = 0;
-  
+
   signal(SIGINT, sig_handler);
   printf( "%s\n", __BASEFILE__);
-
   if (!bcm2835_init()) {
     fprintf( stderr, "%s bcm2835_init() Failed\n\n", __BASEFILE__ );
     return 1;
   }
-  
+
   printf( "RF95 CS=GPIO%d", RF_CS_PIN);
 
 #ifdef RF_LED_PIN
@@ -84,13 +96,13 @@ int main (int argc, const char* argv[] )
 
 #ifdef RF_IRQ_PIN
   printf( ", IRQ=GPIO%d", RF_IRQ_PIN );
-  // IRQ Pin input/pull down 
+  // IRQ Pin input/pull down
   pinMode(RF_IRQ_PIN, INPUT);
   bcm2835_gpio_set_pud(RF_IRQ_PIN, BCM2835_GPIO_PUD_DOWN);
 #endif
-  
+
 #ifdef RF_RST_PIN
-  printf( ", RST=GPIO%d", RF_RST_PIN );
+ printf( ", RST=GPIO%d", RF_RST_PIN );
   // Pulse a reset on module
   pinMode(RF_RST_PIN, OUTPUT);
   digitalWrite(RF_RST_PIN, LOW );
@@ -110,9 +122,9 @@ int main (int argc, const char* argv[] )
     printf( "\nRF95 module seen OK!\r\n");
 
 #ifdef RF_IRQ_PIN
-    // Since we may check IRQ line with bcm_2835 Rising edge detection
+ // Since we may check IRQ line with bcm_2835 Rising edge detection
     // In case radio already have a packet, IRQ is high and will never
-    // go to low so never fire again 
+    // go to low so never fire again
     // Except if we clear IRQ flags and discard one if any by checking
     rf95.available();
 
@@ -123,16 +135,16 @@ int main (int argc, const char* argv[] )
     // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
     // The default transmitter power is 13dBm, using PA_BOOST.
-    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
     // you can set transmitter powers from 5 to 23 dBm:
-    //rf95.setTxPower(23, false); 
+    //rf95.setTxPower(23, false);
     // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
     // transmitter RFO pins and not the PA_BOOST pins
-    // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
+    // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
     // Failure to do that will result in extremely low transmit powers.
     //rf95.setTxPower(14, true);
 
-    rf95.setTxPower(14, false); 
+    rf95.setTxPower(14, false);
 
     // You can optionally require this module to wait until Channel Activity
     // Detection shows no activity on the channel before transmitting by setting
@@ -145,12 +157,11 @@ int main (int argc, const char* argv[] )
     // This is our Node ID
     rf95.setThisAddress(RF_NODE_ID);
     rf95.setHeaderFrom(RF_NODE_ID);
-    
+
     // Where we're sending packet
-    rf95.setHeaderTo(RF_GATEWAY_ID);  
+    rf95.setHeaderTo(RF_GATEWAY_ID);
 
     printf("RF95 node #%d init OK @ %3.2fMHz\n", RF_NODE_ID, RF_FREQUENCY );
-
     last_millis = millis();
 
     //Begin the main body of code
@@ -159,30 +170,53 @@ int main (int argc, const char* argv[] )
       //printf( "millis()=%ld last=%ld diff=%ld\n", millis() , last_millis,  millis() - last_millis );
 
       // Send every 5 seconds
-      if ( millis() - last_millis > 5000 ) {
+      if ( millis() - last_millis > 1000 ) {
         last_millis = millis();
 
 #ifdef RF_LED_PIN
         led_blink = millis();
         digitalWrite(RF_LED_PIN, HIGH);
 #endif
-        
+        ifstream  myfile ("/home/pi/gps.txt") ;
+        ifstream  myfile2 ("/home/pi/sensor.txt");
         // Send a message to rf95_server
-        uint8_t data[] = "Hi Raspi!";
-        uint8_t len = sizeof(data);
-        
-        printf("Sending %02d bytes to node #%d => ", len, RF_GATEWAY_ID );
-        printbuffer(data, len);
+        while (getline (myfile,line)){
+         new1=line;
+        }
+        while (getline (myfile2,line2)) {
+        new2=line2;
+        }
+        const uint8_t *data1 = reinterpret_cast<const uint8_t*>(new1.c_str());
+        const uint8_t *data2 = reinterpret_cast<const uint8_t*>(new2.c_str());
+//////////////////////////////////////////////
+/*       ofstream myfile3("/home/pi/data.txt"); 
+       if (myfile3.open()){
+        myfile3 << " data " << data1<< " " << data2 <<std::endl;   usleep(1000000);}
+ */
+//////////////////////////////////////////////
+/*        ifstream myfile3 ("/home/pi/data.txt");
+        // Send a message to rf95_server
+        while (getline (myfile3,line3)){
+         new3=line3;
+        }
+*/
+        //const uint8_t *data3 = reinterpret_cast<const uint8_t*>(new3.c_str());
+        uint8_t len1 = 100;
+        uint8_t len2 = 100;
+        uint8_t len3 = 100;
+        printf("Sending %02d bytes to node #%d => ", len1, len2, RF_GATEWAY_ID );
+        cout <<data1<< endl;
         printf("\n" );
-        rf95.send(data, len);
+        rf95.send(data1,len1);
+        //rf95.send(data2,len2);
         rf95.waitPacketSent();
 /*
         // Now wait for a reply
         uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
         uint8_t len = sizeof(buf);
 
-        if (rf95.waitAvailableTimeout(1000)) { 
-          // Should be a reply message for us now   
+        if (rf95.waitAvailableTimeout(1000)) {
+          // Should be a reply message for us now
           if (rf95.recv(buf, &len)) {
             printf("got reply: ");
             printbuffer(buf,len);
@@ -194,7 +228,7 @@ int main (int argc, const char* argv[] )
           printf("No reply, is rf95_server running?\n");
         }
 */
-        
+
       }
 
 #ifdef RF_LED_PIN
@@ -204,7 +238,7 @@ int main (int argc, const char* argv[] )
         digitalWrite(RF_LED_PIN, LOW);
       }
 #endif
-      
+
       // Let OS doing other tasks
       // Since we do nothing until each 5 sec
       bcm2835_delay(100);
@@ -218,4 +252,3 @@ int main (int argc, const char* argv[] )
   bcm2835_close();
   return 0;
 }
-
