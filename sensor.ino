f023bbdf6f0a25384a69f9894b5db3d30b618a1f
Aldi Wahyu Saragih <49329871+asengsaragih@users.noreply.github.com>
2020-03-31 21:31:27 +0700

    Update sensor.ino

--- sensor.ino
+++ sensor.ino
@@ -1,59 +1,65 @@
-
-int TURBINE;      //pengukuran SINYAL data yang bersifat incremental
-int HSensor = 2; //nama alias pada pin 2
+const int inoutansensor = A0;
+int TURBINE;
+int HSensor = 2;
 int Calc;
-const int analogInPin = A0;
 int buzz = 3;
-int sensorValue = 0;
-void speedrpm ()    //fungsi penghitungan dan interrupt
-{
-  TURBINE++; //bersifat incrementing (dengan mode falling edge)
+
+void speedrpm() {
+  TURBINE++;
 }
 
-void setup()
-{
-  pinMode(HSensor, INPUT); //inisialisasi sebagai input
-  Serial.begin(9600);
-  pinMode(A0, INPUT);
-  attachInterrupt(0, speedrpm, RISING); //cara penulisan perintah interrupt
+void setup() {
+  pinMode(HSensor, INPUT);
+  pinMode(inputsensor, INPUT);
   pinMode(buzz, OUTPUT);
+  attachInterrupt(0, speedrpm, RISING);
+  Serial.begin(9600);
+  digitalWrite(buzz, LOW);
 }
 
-void loop ()
-{
+void loop() {
+  int value = analogRead(inputsensor);
 
-  sensorValue = analogRead(analogInPin);
-  Serial.print("WLS = " );
-  Serial.print(sensorValue * 100 / 1024);
-  Serial.print("cm");
-  TURBINE = 0; //data awal = 0
-  sei(); //perintah aktifnya mode interrupt
-  delay (1000); //nilai delay 1 detik
-  cli(); //perintah untuk matinya program interrupt
-  Calc = (TURBINE * 1 / 7.5); //Pulsa * 60 / 7.5
+  float cm = (value * 4.0) / 600.0;
+  int x = abs(cm);
 
-  //satuan FLOW RATE benda cair yaitu L / hour
+  TURBINE = 0;
+  sei();
+  delay(1000);
+  cli();
+  Calc = (TURBINE * 1 / 7.5);
+
+  Serial.print(Calc, DEC);
+  Serial.print(" L/m ");
+  Serial.print(cm);
+  Serial.print(" cm");
+
+  if ((Calc, DEC) < 1) {
+    if (cm < 2) {
+      Serial.print(" Status : 1");
+    } else if (cm <= 3.5) {
+      Serial.print(" Status : 2");
+    } else {
+      Serial.print(" Status : 2");
+    }
+  } else if ((Calc, DEC) < 2.5) {
+    if (cm < 2) {
+      Serial.print(" Status : 1");
+    } else if (cm <= 3.5) {
+      Serial.print(" Status : 2");
+    } else {
+      Serial.print(" Status : 3");
+    }
+  } else {
+    if (cm < 2) {
+      Serial.print(" Status : 1");
+    } else if (cm <= 3.5) {
+      Serial.print(" Status : 2");
+    } else {
+      Serial.print(" Status : 3");
+    }
+  }
+
   Serial.println("");
-  Serial.print(  "WFS = " );
-  Serial.print(Calc, DEC); //menampilkan hasil pembacaan kalkulasi flow rate dalam bentuk dec di serial monitor
-  Serial.print(" L/m"); //Tampilkan L / hour pada baris baru
-
-  if (sensorValue <= 2) {
-    digitalWrite(buzz, LOW);
-    Serial.println(" Status : AMAN");
-  }
-  if (sensorValue > 2 && sensorValue <= 3.5) {
-    Serial.println(" Status : Siaga");
-    digitalWrite(buzz, HIGH);
-    delay(1000);
-    digitalWrite(buzz, LOW);
-    delay(1000);
-  }
-  if (sensorValue > 3.5) {
-    Serial.println(" Status : Bahaya");
-    digitalWrite(buzz, HIGH);
-    delay(2000);
-    digitalWrite(buzz, LOW);
-    delay(2000);
-  }
+  delay(1000)
 }
