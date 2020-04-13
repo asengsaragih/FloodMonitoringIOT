2c5718cfb6d0c54a56eaaed9405c53b6adb05c83
aldi saragih <aldiwahyu.saragih@gmail.com>
2020-04-14 03:15:39 +0700

    edit gpsd main.cpp

--- Node/gpds_example/main.cpp
+++ Node/gpds_example/main.cpp
@@ -1,3 +1,4 @@
+
 /********************************************
  *  Gpsd client example
  *  created by s.farmbauer@red-beard-code.de
@@ -7,26 +8,21 @@
 #include <iostream>
 #include <thread>
 #include "GpsPosition.h"
-
-//library yang ditambah
 #include <fstream>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "rs232.h"
 
+#include <string>
 using namespace std;
 
 char* no = new char ();
 string test;
 #define BUF_SIZE 128
 
-
-
 //Controlling the thread flow
 bool gReadGps = true;
-
-
 
 /// Thread which reads all position form the gpsd
 /// server
@@ -54,25 +50,27 @@ void read_gpsd_data() {
     }
         
 }
-
+int n;
 ///Starts the thread which reads the position and stores
 ///it in a Contaier
-int main(void)
+int main(int argc, const char* argv[])
 {
-    std::thread gps_thread(read_gpsd_data);
     std::string line;
-	std::string new1;
-	ifstream myfile2 ("/home/pi/sensor.txt");
-	
-    while (true) {
-		
-		while (getline(myfile2, line)) {
-			new1 = line;
-		}
-		
-		const  uint8_t *data = reinterpret_cast<const uint8_t*>(new1.c_str());
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        GPSPosition last_pos = GPSPosition::get_last_3d_fix_value();
+    std::string new1;
+    std::thread gps_thread(read_gpsd_data);
+
+    for (int i = 0; i < 9999999999999999999999999999999999999999; i++)
+    {
+        /* code */
+        ifstream myfile2 ("/home/pi/sensor.txt");
+
+        while (getline (myfile2,line)) {
+            new1=line;
+        }
+
+        const  uint8_t *data = reinterpret_cast<const uint8_t*>(new1.c_str());
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	    GPSPosition last_pos = GPSPosition::get_last_3d_fix_value();
         time_t now_c = chrono::system_clock::to_time_t(last_pos.get_time());
         now_c = now_c + 25200;
         strftime(no, 19, "%d/%m/%y %H:%M:%S", localtime(&now_c));
@@ -81,20 +79,21 @@ int main(void)
                   << " Lat: " << last_pos.get_latitude()
                   << " Lon: " << last_pos.get_longitude()
                   << " Alt: " << last_pos.get_altitude()
-                  << "  " <<data<<std::endl;
-                  
-        ofstream myfile("/home/pi/gps.txt");
-		if (myfile.is_open()) {
-			myfile << " Time: " << test
-			<< " Lat: " << last_pos.get_latitude()
-			<< " Lon: " << last_pos.get_longitude()
-			<< " Alt: " << last_pos.get_altitude() 
-			<< "   " <<data<<std::endl;
-			usleep(1000000);
-		} else {
-			printf("unable to open gps.txt or sensor.txt");
-		}
-	}
+                  << "  "<<data<<std::endl;
+        ofstream myfile("/home/pi/gps.txt"); 
+
+        if (myfile.is_open()) {
+            myfile << " Time: " << test
+            << " Lat: " << last_pos.get_latitude()
+            << " Lon: " << last_pos.get_longitude()
+            << " Alt: " << last_pos.get_altitude() 
+            <<"   "<<data<<std::endl;
+            usleep(1000000);
+        } else {
+            printf("unable to open gps.txt or sensor.txt");
+        }
+    }
+    
     gReadGps = false;
     gps_thread.join();
     return 0;
